0f9d3b0344a28815d13774cd8d1c83f49b3463dd
Iryna Hulko <irinagulko13@gmail.com>
2020-02-22 17:06:00 +0300

    feat:reverse-int solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    nPos = Math.abs(n);
+    var nStr = nPos.toString(); 
+    var strArray = nStr.split(""); 
+    var revArray = strArray.reverse(); 
+    var rev = revArray.join(""); 
+    return Number(rev);
 }
